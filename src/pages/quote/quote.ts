a5feadd00fa0b2d74cf9bd2b1cbd8e61ed2c58d2
arnulfo nunez <arnulfo nunez>
2017-03-01 22:32:38 -0500

    this is a test commit

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -2,12 +2,7 @@ import { ViewController } from 'ionic-angular';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the Quote page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-quote',
   templateUrl: 'quote.html'
